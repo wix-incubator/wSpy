c0cd425283992d5172db63212854ad2afb0d378d
Yinon Cohen <yinonc@wix.com>
2019-01-23 17:28:29 +0200

    trying to fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -17,12 +17,7 @@ module.exports = {
 			{
 				test: /\.js$/,
 				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['es2015']
-					}
-				}
+				use: ['babel-loader']
 			}
 		]
 	},
