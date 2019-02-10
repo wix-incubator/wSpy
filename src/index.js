235ba6531c0b9fd6c85ce045ba816bb502c25b1e
Yinon Cohen <yinonc@wix.com>
2019-02-10 11:34:12 +0200

    adding init nodeHost

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const browserHost = require('./hosts/wSpyBrowserHost')
 const workerHost = require('./hosts/wSpyWorkerHost')
+const nodeHost = require('./hosts/wSpyNodeHost')
 module.exports = {
 	initBrowserHost: options => browserHost.init(options),
-	initWorkerHost: options => workerHost.init(options)
+	initWorkerHost: options => workerHost.init(options),
+	initNodeHost: options => nodeHost.init(options)
 }
