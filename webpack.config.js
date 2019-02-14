7fc61b1e9775e16d1e1e570fa8e6fdd6a0dd9fcf
Shai Ben-Yehuda <shaiby@wix.com>
2019-02-14 11:40:09 +0200

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path')
+
+module.exports = [{
+  mode: 'development',
+  entry: path.resolve(__dirname, 'dist/index.js'),
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'wspy-all-debug.js'
+    }
+},
+{
+  entry: path.resolve(__dirname, 'dist/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'wspy-all.js'
+  }
+}]
+
+
