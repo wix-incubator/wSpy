4f9396c0683392cc64472fe348d3cd831ffc7b37
Yinon Cohen <yinonc@wix.com>
2019-02-14 15:09:48 +0200

    fixing lint

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,19 @@
 const path = require('path')
 
 module.exports = [{
-  mode: 'development',
-  entry: path.resolve(__dirname, 'dist/index.js'),
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'wspy-all-debug.js'
-    }
+	mode: 'development',
+	entry: path.resolve(__dirname, 'dist/index.js'),
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'wspy-all-debug.js'
+	}
 },
 {
-  entry: path.resolve(__dirname, 'dist/index.js'),
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'wspy-all.js'
-  }
+	entry: path.resolve(__dirname, 'dist/index.js'),
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'wspy-all.js'
+	}
 }]
 
 
